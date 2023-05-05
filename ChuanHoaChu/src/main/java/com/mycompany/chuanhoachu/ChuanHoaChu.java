16cda4fd6ffb0ff3ca738748638b363c0b612781
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-05-05 17:52:09 +0700

    Add files via upload

---
+++ ChuanHoaChu/src/main/java/com/mycompany/chuanhoachu/ChuanHoaChu.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.chuanhoachu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nguyen Anh Tu
+ */
+public class ChuanHoaChu {
+
+    public static void main(String[] args) {
+        String str;
+
+        String ketqua = "";
+        System.out.println("Nhap chuoi: ");
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        str = str.replaceAll("\\s+", " ");
+        str = str.trim();
+        str = str.toLowerCase();
+        String temp[] = str.split(" ");
+        for (int i = 0; i < temp.length; i++) {
+            ketqua += String.valueOf(temp[i].charAt(0)).toUpperCase() + temp[i].substring(1);
+            if (i < temp.length - 1) 
+                ketqua += " ";
+        }
+        System.out.println(ketqua);
+    }
+}
