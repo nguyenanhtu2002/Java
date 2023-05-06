dfa86aa07ee1496b6606d50b09f1e79d084392ad
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-05-06 22:43:39 +0700

    Add files via upload

---
+++ SumNumber/src/main/java/com/mycompany/sumnumber/SumNumber.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sumnumber;
+
+/**
+ *
+ * @author Nguyen Anh Tu
+ */
+public class SumNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
