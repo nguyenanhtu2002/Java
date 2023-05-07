e6f227cc31765c1c8419ea8be14a449df87a3d44
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-05-07 21:56:20 +0700

    Update ElementPositionInArray.java

--- ElementPositionInArray/src/main/java/com/mycompany/elementpositioninarray/ElementPositionInArray.java
+++ ElementPositionInArray/src/main/java/com/mycompany/elementpositioninarray/ElementPositionInArray.java
@@ -1,50 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.elementpositioninarray;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Nguyen Anh Tu
- */
 public class ElementPositionInArray {
 
     public static void main(String[] args) {
-        System.out.println("Nhap so phan tu mang");
-        int n = 0;
+        System.out.println("Enter the number of array elements");
+        int sizeArray = 0;
         int a, b;
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        int arr[] = new int[n + 1];
-        inputArray(n, arr);
-        System.out.println("nhap a");
+        sizeArray = sc.nextInt();
+        int arr[] = new int[sizeArray + 1];
+        inputArray(sizeArray, arr);
+        System.out.println("input number a");
         a = sc.nextInt();
-        System.out.println("nhap b");
+        System.out.println("input number b");
         b = sc.nextInt();
-        Position(n, arr, a - b);
+        Position(sizeArray, arr, a - b);
     }
 
-    public static void inputArray(int n, int arr[]) {
+    public static void inputArray(int sizeArray, int arr[]) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < sizeArray; i++) {
             System.out.printf("arr[%d] = ", i);
             arr[i] = sc.nextInt();
         }
     }
 
-    public static void Position(int n, int arr[], int x) {
+    public static void Position(int n, int arr[], int key) {
         int check = 0;
         for (int i = 0; i < n; i++) {
-            if (arr[i] == x) {
-                System.out.printf("vi tri la: %d", i);
+            if (arr[i] == key) {
+                System.out.printf("position at: arr[%d]", i);
                 check += 1;
             }
         }
         if (check == 0) {
-            System.out.println("khong tim thay");
+            System.out.println("can not find");
         }
     }
 }
+//nhập từ bàn phím 1 mảng, 2 số a và b in ra vị trí phần tử a-b có trong mảng
