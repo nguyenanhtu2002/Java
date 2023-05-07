71236a12d3a3001e3631e880722c9901878f1b93
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-05-07 21:48:56 +0700

    Update MixTwoArray.java

--- MixTwoArray/src/main/java/com/mycompany/mixtwoarray/MixTwoArray.java
+++ MixTwoArray/src/main/java/com/mycompany/mixtwoarray/MixTwoArray.java
@@ -1,59 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.mixtwoarray;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- *
- * @author Nguyen Anh Tu
- */
 public class MixTwoArray {
 
     public static void main(String[] args) {
-        int n, m = 0;
+        int sizeArr1, sizeArr2 = 0;
         int a, b;
-        System.out.println("Nhap so phan tu mang 1: ");
+        System.out.println("Enter the number of array elements one: ");
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        int arr1[] = new int[n + 1];
-        inputArray(n, arr1);
+        sizeArr1 = sc.nextInt();
+        int arr1[] = new int[sizeArr1 + 1];
+        inputArray(sizeArr1, arr1);
         
-        System.out.println("Nhap so phan tu mang 2: ");
-        m = sc.nextInt();   
-        int arr2[] = new int[m + 1];      
-        inputArray(m, arr2);
+        System.out.println("Enter the number of array elements two: ");
+        sizeArr2 = sc.nextInt();   
+        int arr2[] = new int[sizeArr2 + 1];      
+        inputArray(sizeArr2, arr2);
         
-        int arr3[] = new int[n + m];
-        mixTwoArray(n,m,arr1,arr2,arr3);
+        int arr3[] = new int[sizeArr1 + sizeArr2];
+        mixTwoArray(sizeArr1,sizeArr2,arr1,arr2,arr3);
     }
 
-    public static void inputArray(int n, int arr[]) {
+    public static void inputArray(int sizeArr1, int arr[]) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < sizeArr1; i++) {
             System.out.printf("\n arr[%d] = ", i);
             arr[i] = sc.nextInt();
         }
     }
 
-    public static void outputArray(int n, int arr[]) {
-        for (int i = 0; i < n; i++) {
+    public static void outputArray(int sizeArr1, int arr[]) {
+        for (int i = 0; i < sizeArr1; i++) {
             System.out.printf(" arr[%d] = %d ", i, arr[i]);
         }
     }
-    public static void mixTwoArray(int n, int m, int arr1[], int arr2[], int arr3[]){
-        for(int i=0;i<n;i++){
+    public static void mixTwoArray(int sizeArr1, int sizeArr2, int arr1[], int arr2[], int arr3[]){
+        for(int i=0;i<sizeArr1;i++){
             arr3[i]= arr1[i];      
         }
-        for(int i=0;i<m;i++){
-            arr3[n+i] = arr2[i];
+        for(int i=0;i<sizeArr2;i++){
+            arr3[sizeArr1+i] = arr2[i];
         }
         Arrays.sort(arr3);
-        System.out.println("sau khi tron 2 mang va sap xep \n");
-        outputArray(n+m, arr3);
+        System.out.println("result \n");
+        outputArray(sizeArr1+sizeArr2, arr3);
     }
    
 
