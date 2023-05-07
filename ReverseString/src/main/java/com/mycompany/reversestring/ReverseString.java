01953455a49e72334737996b9e37a603547572ba
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-05-07 21:18:36 +0700

    Update ReverseString.java

--- ReverseString/src/main/java/com/mycompany/reversestring/ReverseString.java
+++ ReverseString/src/main/java/com/mycompany/reversestring/ReverseString.java
@@ -2,31 +2,25 @@ package com.mycompany.reversestring;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Nguyen Anh Tu
- */
 public class ReverseString {
 
     public static void main(String[] args) {
-        System.out.println("Nhap chuoi");
+        System.out.println("input string");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         reverseStr(str);
     }
 
     public static void reverseStr(String str) {
-        String ketqua = "";
+        String result = "";
         String arr[] = str.trim().split("\\s+");
         for (int i = arr.length - 1; i >= 0; i--) {
-            ketqua += arr[i];
+            result += arr[i];
             if (i > 0) {
-                ketqua += " ";
+                result += " ";
             }
         }
-        System.out.printf("%s", ketqua);
+        System.out.printf("%s", result);
     }
 
 }
-//nhập vào 1 chuỗi in ra chuỗi đảo ngược (xóa bỏ dấu cách thừa)
-//ví dụ( “     hello     world   “) →(“world hello”)
