9bc8bcbf7c616cf2cc68dff2d4fd5134a9b316f6
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-05-07 21:09:58 +0700

    Update SortArray.java

--- SortArray/src/main/java/com/mycompany/sortarray/SortArray.java
+++ SortArray/src/main/java/com/mycompany/sortarray/SortArray.java
@@ -1,51 +1,45 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.sortarray;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Nguyen Anh Tu
- */
 public class SortArray {
 
     public static void main(String[] args) {
-        int n = 0;
+        int sizeArr = 0;
         Scanner sc = new Scanner(System.in);
-        do{
-        System.out.println("nhap so phan tu mang: ");
-        n = sc.nextInt();    
-        }
-        while(n<0);
-        int arr[] = new int[n + 1];
-        inputArray(n, arr);
-        System.out.println("mang vua nhap:");
-        outputArray(n, arr);
-        sortUpAscending(n, arr);
-        sortDescending(n,arr);
+        do {
+            System.out.println("Enter the number of array elements: ");
+            sizeArr = sc.nextInt();
+        } while (sizeArr < 0);
+        int arr[] = new int[sizeArr + 1];
+        inputArray(sizeArr, arr);
+        System.out.println("Array:");
+        outputArray(sizeArr, arr);
+        sortUpAscending(sizeArr, arr);
+        sortDescending(sizeArr, arr);
     }
 
-    public static void inputArray(int n, int arr[]) {
+    public static void inputArray(int sizeArr, int arr[]) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < sizeArr; i++) {
             System.out.printf("arr[%d]= ", i);
             arr[i] = sc.nextInt();
         }
     }
 
-    public static void outputArray(int n, int arr[]) {
-        for (int i = 0; i < n; i++) {
+    public static void outputArray(int sizeArr, int arr[]) {
+        for (int i = 0; i < sizeArr; i++) {
             System.out.printf(" arr[%d] = %d ", i, arr[i]);
         }
     }
 
-    public static void sortUpAscending(int n, int arr[]) {
+    public static void sortUpAscending(int sizeArr, int arr[]) {
         int temp;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < sizeArr; i++) {
+            for (int j = i + 1; j < sizeArr; j++) {
                 if (arr[j] < arr[i]) {
                     temp = arr[j];
                     arr[j] = arr[i];
@@ -53,13 +47,14 @@ public class SortArray {
                 }
             }
         }
-        System.out.println("\n mang sau khi sap xep tang dan ");
-        outputArray(n, arr);
+        System.out.println("\n Sort Up Ascending  ");
+        outputArray(sizeArr, arr);
     }
-        public static void sortDescending(int n, int arr[]) {
+
+    public static void sortDescending(int sizeArr, int arr[]) {
         int temp;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < sizeArr; i++) {
+            for (int j = i + 1; j < sizeArr; j++) {
                 if (arr[j] > arr[i]) {
                     temp = arr[j];
                     arr[j] = arr[i];
@@ -67,7 +62,7 @@ public class SortArray {
                 }
             }
         }
-        System.out.println("\n mang sau khi sap xep giam dan");
-        outputArray(n, arr);
+        System.out.println("\n sort descending");
+        outputArray(sizeArr, arr);
     }
 }
