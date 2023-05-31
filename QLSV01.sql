2e979d7bbdd0956b6c651cbc8bb75dc59688e72f
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-05-31 17:55:04 +0700

    Add files via upload

---
+++ QLSV01.sql
@@ -0,0 +1,12 @@
+﻿create database QLSV01
+use QLSV01
+CREATE TABLE students (
+    id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    PRIMARY KEY (id)
+)
+insert into students values
+(1,N'Nguyễn Anh Tú','abc@gmail.com','039788888')
+select * from students
