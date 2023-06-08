e56c85107b5b6139c6ee33a6d618094b75ffce0e
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-06-08 18:42:14 +0700

    Update DataSource.java

--- ConnectionPoolF/src/dao/DataSource.java
+++ ConnectionPoolF/src/dao/DataSource.java
@@ -5,17 +5,15 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Objects;
+
 public class DataSource {
-
-    private static DataSource instance;
+    private static HikariConfig config = new HikariConfig();
     private static HikariDataSource ds;
-     private static DataSource dataSource;
-
-    private DataSource() {
-        HikariConfig config = new HikariConfig();
+    private static DataSource dataSource;
+    static {
         config.setJdbcUrl("jdbc:sqlserver://localhost:1433;databaseName=QLSV01");
         config.setUsername("sa");
-        config.setPassword("1234");
+        config.setPassword("123456");
         config.setMaximumPoolSize(10);
         config.setMinimumIdle(5);
         config.setIdleTimeout(30000);
@@ -23,14 +21,18 @@ public class DataSource {
         ds = new HikariDataSource(config);
     }
 
+    private DataSource() {
+    }
+
+    public Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
     public static DataSource getInstance() {
         if (Objects.isNull(dataSource)) {
             dataSource = new DataSource();
         }
         return dataSource;
-    }
-        public Connection getConnection() throws SQLException {
-        return ds.getConnection();
     }
 
 }
